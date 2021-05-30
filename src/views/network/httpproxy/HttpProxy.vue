42ed6ea0ebae15c274a76ed6dc840132df311524
Lcsunm <tri@lcsunm.cc>
2021-05-30 19:51:04 +0800

    -细节修改

--- src/views/network/httpproxy/HttpProxy.vue
+++ src/views/network/httpproxy/HttpProxy.vue
@@ -5,8 +5,9 @@
                 label="WIFI" placeholder="请选择" disabled
                 @click.native="onClick_SelectWifi"></Input>
             <!-- <div class="SelectWifi " v-focusable @click="onClick_SelectWifi">选择Wifi:{{mWifi}}</div> -->
-            <Input v-focusable v-model="mIpAndPort"
-                label="代理地址" placeholder="示例：127.0.0.1:8080"></Input>
+            <Input v-focusable v-model="mIpAndPort" ref="IpAndPort"
+                label="代理地址" placeholder="xxx.xxx.xxx.xxx:xxxx"
+                @click.native="onClick_IpAndPort" @onFocus.native="onFocus('IpAndPort')" @onBlur.native="onBlur('IpAndPort')"></Input>
         </div>
     </Page>
 </template>
@@ -17,10 +18,17 @@ import { PageOptions } from "@/components/Page.vue";
 import MyPage from "@/common/MyPage";
 import R from "@/common/R";
 
+type Type = "IpAndPort" | null;
+
 @Component
 export default class HttpProxy extends MyPage {
 
     static REQUEST_CODE_SELECT_WIFI = 1;
+
+    $refs!: {
+        IpAndPort: Vue;
+        Items: Element;
+    }
 
     @Prop({ default: "标题" }) readonly title!: string;
 
@@ -42,7 +50,9 @@ export default class HttpProxy extends MyPage {
                     get left() {
                         return context.isVaild ? (context.mIpAndPort ? "设置代理" : "清空代理") : "";
                     },
-                    center: "",
+                    get center() {
+                        return context.mFocusedName == "IpAndPort" ? "追加符号" : "";
+                    },
                     right: "返回",
                     on: {
                         keyPress: {
@@ -65,6 +75,7 @@ export default class HttpProxy extends MyPage {
 
     mWifi: any = null;
     mIpAndPort = "";
+    mFocusedName = "";
 
     get isVaild() {
         return this.mWifi;
@@ -78,6 +89,21 @@ export default class HttpProxy extends MyPage {
         this.startPageForResult({
             name: "SelectWifi",
         }, HttpProxy.REQUEST_CODE_SELECT_WIFI)
+    }
+
+    onClick_IpAndPort() {
+        if (!this.mIpAndPort) {
+            return;
+        }
+        const reg1 = /(^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$)|(^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$)|(^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$)/;
+        const reg2 = /^(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])\.(\d{1,2}|1\d\d|2[0-4]\d|25[0-5])$/;
+        if (reg1.test(this.mIpAndPort)) {
+            this.mIpAndPort += ".";
+        } else if (reg2.test(this.mIpAndPort)) {
+            this.mIpAndPort += ":";
+        } else {
+            // this.$prompt.showToast("无需追加符号");
+        }
     }
 
     onClick_Set() {
@@ -105,6 +131,16 @@ export default class HttpProxy extends MyPage {
         request.onerror = (error) => {
             this.$prompt.showToast("设置失败");
         };
+    }
+
+    onFocus(type: Type) {
+        if (type == "IpAndPort") {
+            this.mFocusedName = "IpAndPort";
+        }
+    }
+
+    onBlur(type: Type) {
+        this.mFocusedName = "";
     }
 
     onPageResult(requestCode: number, resultCode: number, data: any) {
