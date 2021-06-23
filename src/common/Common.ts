744d8cc9317b3a03f409c3d31d5107a7b480f473
Lcsunm <tri@lcsunm.cc>
2021-06-23 22:16:19 +0800

    -const->let

--- src/common/Common.ts
+++ src/common/Common.ts
@@ -10,7 +10,7 @@ Date.prototype.format = function (fmt) {
         S: this.getMilliseconds(), //毫秒
     };
     if (/(y+)/.test(fmt)) fmt = fmt.replace(RegExp.$1, (this.getFullYear() + "").substr(4 - RegExp.$1.length));
-    for (const k in o) if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
+    for (let k in o) if (new RegExp("(" + k + ")").test(fmt)) fmt = fmt.replace(RegExp.$1, RegExp.$1.length == 1 ? o[k] : ("00" + o[k]).substr(("" + o[k]).length));
     return fmt;
 };
 
