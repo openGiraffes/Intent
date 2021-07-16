0dec8bea8bf099578c9455e352395e951d25a06f
Lcsunm <tri@lcsunm.cc>
2021-07-16 20:39:09 +0800

    -增加文件选择器

--- src/views/picker/PickerIndex.vue
+++ src/views/picker/PickerIndex.vue
@@ -2,15 +2,15 @@
     <Page class="PickerIndex" :options="mPageOptions">
         <div class="Items wh-100 scroll-y" ref="Items" v-keep-scroll>
             <div class="Item flex-h middle" v-focusable v-for="(item,index) in mItems" :key="index" ref="Item"
-                :folder="item.type === undefined"
-                @click="onClick_Item(item)">
+                :folder="!!item.path"
+                @click="onClick_Item(item,index)">
                 <div class="Icon"></div>
                 <div class="flex-v flex-1">
                     <div class="Name">{{item.name}}</div>
-                    <div v-if="item.type !== undefined" class="Bottom flex-h">
+                    <!-- <div v-if="!item.path" class="Bottom flex-h">
                         <div class="Size">{{formatSize(item.size)}}</div>
                         <div class="Date">{{Format.date(item.lastModifiedDate)}}</div>
-                    </div>
+                    </div> -->
                 </div>
             </div>
         </div>
@@ -47,7 +47,7 @@ export default class PickerIndex extends MyPage {
                     on: {
                         keyPress: {
                             softRight: () => {
-                                context.back();
+                                context.mActivityRequest?.postError("cancel");
                             },
                             "Backspace": (e) => {
                                 this.onClick_Back();
@@ -65,13 +65,32 @@ export default class PickerIndex extends MyPage {
     }
 
     mItems: any[] = [];
-    mPaths: any[][] = [];
+    mPaths: {
+        items: any[];
+        index: number;
+    }[] = [];
+    mActivityRequest: any;
+
+    mounted() {
+        navigator.mozSetMessageHandler?.("activity", (activityRequest) => {
+            var option = activityRequest.source;
+            if (option.name === "pick") {
+                this.mActivityRequest = activityRequest;
+            }
+        });
+    }
 
     onStart() {
         this.$super.onStart();
         this.$tv.scrollEl = this.$refs.Items;
         if (isDev) {
-            this.mItems.push({ name: "文件夹", }, { name: "文件", type: "", size: 744654456, lastModifiedDate: Date.now() })
+            let list: any[] = [
+                { name: "bbb", path: "/bbb", },
+                { name: "bbb", type: "", size: 744654456, lastModifiedDate: Date.now() },
+                { name: "aaa", path: "/aaa", },
+            ];
+            list = [...list.filter(o => o.path), ...list.filter(o => !o.path)];
+            this.mItems.push(...list);
         } else {
             this.loadData();
         }
@@ -79,44 +98,58 @@ export default class PickerIndex extends MyPage {
 
     loadData() {
         let storages = navigator.getDeviceStorages("sdcard");
-        this.mPaths.push([]);
+        this.mPaths.push({
+            items: [],
+            index: 0,
+        });
         storages.forEach((e) => {
+            console.log("storages", e);
             this.loadData_Storage(e.getRoot());
         });
     }
 
     loadData_Storage(request: Promise<any>) {
         request.then((res) => {
-            this.mPaths[0].push(res);
+            console.log("storage", res);
+            this.mPaths[0].items.push(res);
             this.refreshItems();
         }).catch((error) => {
-            this.$prompt.showToast("加载失败");
+            this.$prompt.showToast(this.$t("failed-to-load"));
         });
     }
 
     loadData_Path(request: Promise<any>) {
-        request.then((res) => {
-            console.log("res", res);
-            this.mPaths.push(res);
+        request.then((res: any[]) => {
+            if (!res.length) {
+                this.$prompt.showToast(this.$t("empty-folder"));
+                return;
+            }
+            let list = [...res.filter(o => o.path), ...res.filter(o => !o.path)];
+            console.log("res", list);
+            this.mPaths.push({
+                items: list,
+                index: 0,
+            });
             this.refreshItems();
         }).catch((error) => {
-            this.$prompt.showToast("加载失败");
+            this.$prompt.showToast(this.$t("failed-to-load"));
         });
     }
 
     switchItems(items: any[]) {
-        this.mItems = [];
         this.mItems.push(...items);
     }
 
     refreshItems() {
-        this.switchItems(this.mPaths[this.mPaths.length - 1]);
-        // this.$nextTick(() => {
-        //     let item = this.$refs.Item;
-        //     if (item.length) {
-        //         this.$tv.requestFocus(item[0]);
-        //     }
-        // })
+        let path = this.mPaths[this.mPaths.length - 1];
+        this.mItems = [];
+        this.mItems.push(...path.items);
+        this.$nextTick(() => {
+            let item = this.$refs.Item;
+            if (path.index < item.length) {
+                this.$tv.requestFocus(item[path.index]);
+            }
+        })
     }
 
     formatSize(value: number) {
@@ -125,15 +158,26 @@ export default class PickerIndex extends MyPage {
         let srcsize = value;
         index = Math.floor(Math.log(srcsize) / Math.log(1024));
         let size: any = srcsize / Math.pow(1024, index);
-        size = size.toFixed(2);//保留的小数位数
+        size = size.toFixed(2);
         return size + unitArr[index];
     }
 
-    onClick_Item(item: any) {
-        if (item.type !== undefined) {
-            this.$prompt.showToast("文件");
+    onClick_Item(item: any, index: number) {
+        if (!item.path) {
+            let sdcard = navigator.getDeviceStorage(this.mPaths[0].items[this.mPaths[0].index].name);
+            let path = this.mPaths[this.mPaths.length - 2];
+            path = path ? path.items[path.index].name : "";
+            let request = sdcard.get([path, item.name].filter(o => o).join("/"));
+            request.onsuccess = (res) => {
+                var file = res.target.result;
+                this.mActivityRequest?.postResult({ type: file.type, blob: file });
+            };
+            request.onerror = () => {
+                this.$prompt.showToast(this.$t("failed-to-load"));
+            };
             return;
         }
+        this.mPaths[this.mPaths.length - 1].index = index;
         this.loadData_Path(item.getFilesAndDirectories());
     }
 
@@ -153,7 +197,7 @@ export default class PickerIndex extends MyPage {
     position: relative;
     background: rgba($color: #eeeeee, $alpha: 1);
     .Items {
-        // padding: 10px 16px;
+        padding: 10px 0;
         position: relative;
         &::after {
             content: "";
@@ -162,7 +206,7 @@ export default class PickerIndex extends MyPage {
         }
         .Item {
             font-size: 12px;
-            padding: 7px 16px 7px 16px;
+            padding: 5px 16px 5px 16px;
             &.focus {
                 background: rgba($color: $colorPrimary, $alpha: 0.3);
             }
@@ -177,8 +221,8 @@ export default class PickerIndex extends MyPage {
             .Icon {
                 margin-right: 10px;
                 background: #b3c3ce;
-                width: 20px;
-                height: 20px;
+                width: 16px;
+                height: 16px;
                 border-radius: 5px;
             }
             .Name {
